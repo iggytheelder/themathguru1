90452b4bddf4a410f48019baa3ed3f087bc58d7a
iggytheelder <ignacec@goldmail.etsu.edu>
2023-06-27 22:09:31 -0400

    added footer nav

--- components/Footer.js
+++ components/Footer.js
@@ -7,20 +7,34 @@ export default function Footer({ page }) {
   if (page != "newsletter") {
     return (
       <>
-        <footer className={styles.footer}>
-          <h3>Newsletter</h3>
-          <p>
-            We will be lauching on August 25, 2023.
-            Sign up to get special lauch offers and ways to get involved with math education.
-            After the launch, you'll get notified when we have something really special to share with you.
-          </p>
-          <p>
-            <Link href = "/newsletter"><a>Learn More</a></Link>
-          </p>
-          <NewsletterForm/>
+        <footer>
+          <div className={styles.newsletter}>
+            <h3>Newsletter</h3>
+            <p>
+              We will be lauching on August 25, 2023.
+              Sign up to get special lauch offers and ways to get involved with math education.
+              After the launch, you'll get notified when we have something really special to share with you.
+            </p>
+            <p>
+              <Link href = "/newsletter"><a>Learn More</a></Link>
+            </p>
+            <NewsletterForm/>
           {/*
           <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
           */}
+          </div>
+          <div class={styles.footerNav}>
+            <div class={styles.left}>
+              <Link href="/"><a>The Math Guru</a></Link><br/>
+              <Link href="/seek"><a>Find a Tutor</a></Link><br/>
+              <Link href="/become"><a>Become a Tutor</a></Link>
+            </div>
+            <div class={styles.right}>
+              <Link href="/blog"><a>Blog</a></Link><br/>
+              <Link href="/newsletter"><a>Newsletter</a></Link><br/>
+              <Link href="/contact"><a>Contact</a></Link>
+            </div>
+        </div>
         </footer>
       </>
     )
