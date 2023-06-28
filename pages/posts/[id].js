46860ea89f0bd7f6affde7ba265cb10dd3c7d430
iggytheelder <ignacec@goldmail.etsu.edu>
2023-06-28 02:23:56 -0400

    failed to build, fixed error I think

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image'
-import Layout from '@components/layout';
+import Layout from '@components/Layout';
 import Date from '@components/date';
 import ShareBtn from '@components/ShareBtn';
 
