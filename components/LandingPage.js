0b9504e768acdbfba42ca01575c729d2e4480731
iggytheelder <ignacec@goldmail.etsu.edu>
2023-07-15 09:09:32 -0700

    trying to rebuidl landing page

---
+++ components/LandingPage.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function LandingPage() {
+  return (
+    <>
+      <div>
+        Landing Page
+      </div>
+    </>
+  )
+}
