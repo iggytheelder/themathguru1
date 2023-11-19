161728b45e62dd2a0311a420dadb0c88ec29a108
iggytheelder <ignacec@goldmail.etsu.edu>
2023-11-19 13:30:27 -0500

    change pricing

--- pages/index.js
+++ pages/index.js
@@ -42,11 +42,12 @@ export default function Home() {
         <p className = "center">
           <i>The office hours your math professor can't offer.</i>
         </p>
-        <p className = "center">
+        <p className = "center" style={{lineHeight: "50px"}}>
           <a href="#summary">Summary</a>{' '}&bull;{' '}
           <a href="#testimonials">Testimonials</a>{' '}&bull;{' '}
           <a href="#how-it-works">How It Works</a>{' '}&bull;{' '}
           <a href="#pricing">Pricing</a>{' '}&bull;{' '}
+          <a href="#refund-policy">Refund Policy</a>{' '}&bull;{' '}
           <a href="#referrals">Referrals</a>{' '}&bull;{' '}
           <a href="#contact">Contact</a>
         </p>
@@ -87,12 +88,12 @@ export default function Home() {
         <h2 id = "how-it-works">How it works</h2>
         <p>
           Caleb tutors for ANY math class and for any age.
-          Most students that use us for math tutoring are taking a college math class, such as college algebra, calculus, or more advanced math classes.
+          Most students that use us for math tutoring are taking a college math class, such as college algebra, calculus, or a more advanced math classes.
         </p>
         <p>
           After you text Caleb, he will offer a 10-minute consultation call (FREE) before scheduling your first session.
           Feel free to ask any questions that you have.
-          Send Caleb your syllabus and your grades, and we will calculate what you need to score on the remaining exams (FREE).
+          Send Caleb your syllabus and your grades, and Caleb will calculate what you need to score on the remaining exams (FREE).
           Next, schedule a 60 minute tutoring session.
         </p>
         <p>
@@ -101,67 +102,85 @@ export default function Home() {
         </p>
         <p>
           Caleb uses Calendly for scheduling and Venmo for payment.
-          After each session, we will request for payment.
-          No need to send payment before the session.
           Everytime you schedule, you'll receieve an email confirmation which will go on your calendar and a text reminder 1 hour before the session.
         </p>
+        <p>
+          If you are purchasing a single session at a time, Caleb will request for payment after each session.
+          No need to send payment before the session.
+        </p>
+        <p>
+          You may purchase multiple sessions in advance to get a cheaper price per session, see the <a href="#pricing">pricing section</a> for details.
+          Caleb will keep track of how many sessions you have used and you may ask at any time how many you have left.
+        </p>
         <h2 id = "pricing">Pricing</h2>
-        <p>
-          Tutoring sessions are an hour long.
-        </p>
-        <p style={{display: "flex", justifyContent: "space-between"}}>
-          <span>
-          Buy one (1) session at a time:
-          </span>
-          <span>
-            $135 per session
-          </span>
-        </p>
-        <p style={{display: "flex", justifyContent: "space-between"}}>
-          <span>
-          Buy five (5) sessions in advance:
-          </span>
-          <span>
-            $130 per session
-          </span>
-        </p>
-        <p style={{display: "flex", justifyContent: "space-between"}}>
-          <span>
-          Buy ten (10) sessions in advance:
-          </span>
-          <span>
-            $120 per session
-          </span>
-        </p>
-        <p style={{display: "flex", justifyContent: "space-between"}}>
-          <span>
-          Buy twenty (20) sessions in advance:
-          </span>
-          <span>
-            $105 per session
-          </span>
-        </p>
+          <p>
+            Tutoring sessions are an hour long.
+          </p>
+          <p>
+            Want to try Caleb out first? Just do a single session, then purchase a package.
+            That first session will be included in your package.
+          </p>
+          <div className = "pricing-table">
+            <h3 className = "center">
+            </h3>
+            <p style={{display: "flex", justifyContent: "space-between"}}>
+              <span>
+              Sessions
+              </span>
+              <span>
+              Price / Session
+              </span>
+            </p>
+            <p style={{display: "flex", justifyContent: "space-between"}}>
+              <span>
+              1
+              </span>
+              <span>
+              $135
+              </span>
+            </p>
+            <p style={{display: "flex", justifyContent: "space-between"}}>
+              <span>
+              5
+              </span>
+              <span>
+              $130
+              </span>
+            </p>
+            <p style={{display: "flex", justifyContent: "space-between"}}>
+              <span>
+              10
+              </span>
+              <span>
+              $120
+              </span>
+            </p>
+            <p style={{display: "flex", justifyContent: "space-between"}}>
+              <span style={{fontWeight: "bold"}}>
+              20
+              </span>
+              <span style={{fontWeight: "bold"}}>
+              $95
+              </span>
+            </p>
+          </div>
+          <p>
+            The best deal is 20 sessions for $95 per session.
+          </p>
         <div>
-        <table>
-          <tr>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>Country</th>
-          </tr>
-          <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-          </tr>
-          <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-          </tr>
-        </table>
         </div>
+        <h2 id = "refund-policy">
+          Refund Policy
+        </h2>
         <p>
-          Refund policy: When you buy in advance, any sessions you use will be reduced at
+          Refunds are allowed. Any sessions you have used will be charged at the single session price, and the rest will be refunded to you.
+          The following expression represents your refund amount, in dollars:
+        </p>
+        <p className = "center">
+          ( sessions purchased ) &bull; ( price / session ) &mdash; ( sessions used ) &bull; 135
+        </p>
+        <p>
+          You will receive a refund if the above expression is positive.
         </p>
         <h2 id = "referrals">Referrals</h2>
         <p>
